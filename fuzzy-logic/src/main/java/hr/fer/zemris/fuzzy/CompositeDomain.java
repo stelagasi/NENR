f800e40494af05b920c467a97ffc2d345e555758
Stela Gaši <sg50985@fer.hr>
2020-10-05 19:38:50 +0200

    added getFirstCompositeElement in CompositeDomain

--- fuzzy-logic/src/main/java/hr/fer/zemris/fuzzy/CompositeDomain.java
+++ fuzzy-logic/src/main/java/hr/fer/zemris/fuzzy/CompositeDomain.java
@@ -1,5 +1,7 @@
 package hr.fer.zemris.fuzzy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 
 public class CompositeDomain extends Domain {
@@ -11,35 +13,59 @@ public class CompositeDomain extends Domain {
     }
 
     //todo može biti nula domains.length
-//    @Override
-//    public Iterator<DomainElement> iterator() {
-//        return new Iterator<>() {
-//            private int current = 0;
-//
-//            @Override
-//            public boolean hasNext() {
-//                return current < getCardinality();
-//            }
-//
-//            @Override
-//            public DomainElement next() {
-//                for(int i = 0; i < getNumberOfComponents(); i++){
-//                    for(int j = 0; j < getComponent(i).getCardinality(); j++){
-//                        if(i * getNumberOfComponents() + j == current){
-//                            return new DomainElement(g)
-//                        }
-//                    }
-//                }
-//            }
-//        };
-//    }
+    @Override
+    public Iterator<DomainElement> iterator() {
+        return new Iterator<>() {
+            private int current = 0;
+
+            @Override
+            public boolean hasNext() {
+                return current < getCardinality();
+            }
+
+            @Override
+            public DomainElement next() {
+                ArrayList<Integer> values = new ArrayList<>();
+                int numberOfComponents = getNumberOfComponents();
+                for (int i = numberOfComponents - 1; i >= 0; i--) {
+                    if (getComponent(i).getCardinality() > current) {
+                        for (int j = 0; j < numberOfComponents - i; j++) {
+                            if (getComponent(j) instanceof SimpleDomain) {
+                                SimpleDomain simpleComponent = (SimpleDomain) getComponent(j);
+                                values.add(simpleComponent.getFirst());
+                            } else {
+                                CompositeDomain compositeComponent = (CompositeDomain) getComponent(j);
+                                values.addAll(compositeComponent.getFirstCompositeElement());
+                            }
+                        }
+                        //todo dodaj i trenutni član
+                        return new DomainElement(values);
+                    }
+                }
+            }
+        };
+}
+
+    public ArrayList<Integer> getFirstCompositeElement() {
+        ArrayList<Integer> values = new ArrayList<>();
+        for(int i = 0; i < getNumberOfComponents(); i++){
+            if(getComponent(i) instanceof SimpleDomain){
+                SimpleDomain simpleComponent = (SimpleDomain) getComponent(i);
+                values.add(simpleComponent.getFirst());
+            } else {
+                CompositeDomain compositeComponent = (CompositeDomain) getComponent(i);
+                values.addAll(compositeComponent.getFirstCompositeElement());
+            }
+        }
+        return values;
+    }
 
     @Override
     public int getCardinality() {
-        if(domains.length == 0) return 0;
+        if (domains.length == 0) return 0;
 
         int cardinality = 1;
-        for(IDomain domain : domains){
+        for (IDomain domain : domains) {
             cardinality *= domain.getCardinality();
         }
         return cardinality;
