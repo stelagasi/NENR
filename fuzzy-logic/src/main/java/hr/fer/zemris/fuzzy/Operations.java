d89c5af8747125609bdb13e05b35c1e378260295
Stela Gaši <sg50985@fer.hr>
2020-10-08 20:33:28 +0200

    wrote all methods except hamacher norms

--- fuzzy-logic/src/main/java/hr/fer/zemris/fuzzy/Operations.java
+++ fuzzy-logic/src/main/java/hr/fer/zemris/fuzzy/Operations.java
@@ -2,37 +2,49 @@ package hr.fer.zemris.fuzzy;
 
 public class Operations {
 
-    public static IFuzzySet unaryOperation(IFuzzySet fuzzySet, IUnaryFunction unaryFunction){
-        //fuzzySet.getDomain();
-        return null;
+    public static IFuzzySet unaryOperation(IFuzzySet fuzzySet, IUnaryFunction unaryFunction) {
+        if (!(fuzzySet.getDomain() instanceof SimpleDomain)) throw new IllegalArgumentException();
+        SimpleDomain simpleDomain = (SimpleDomain) fuzzySet.getDomain();
+        MutableFuzzySet resultFuzzySet = new MutableFuzzySet(simpleDomain);
+        for (int i = simpleDomain.getFirst(); i < simpleDomain.getLast(); i++) {
+            DomainElement domainElement = new DomainElement(i);
+            resultFuzzySet.set(domainElement, unaryFunction.valueAt(fuzzySet.getValueAt(domainElement)));
+        }
+        return resultFuzzySet;
     }
 
-    public static IFuzzySet binaryOperation(IFuzzySet firstFuzzySet, IFuzzySet secondFuzzySet, IBinaryFunction binaryFunction){
+    public static IFuzzySet binaryOperation(IFuzzySet firstFuzzySet, IFuzzySet secondFuzzySet, IBinaryFunction binaryFunction) {
+        if (!(firstFuzzySet.getDomain().equals(secondFuzzySet.getDomain())) ||
+                !(firstFuzzySet.getDomain() instanceof SimpleDomain)) throw new IllegalArgumentException();
+        SimpleDomain simpleDomain = (SimpleDomain) firstFuzzySet.getDomain();
+        MutableFuzzySet resultFuzzySet = new MutableFuzzySet(simpleDomain);
+        for (int i = simpleDomain.getFirst(); i < simpleDomain.getLast(); i++) {
+            DomainElement domainElement = new DomainElement(i);
+            resultFuzzySet.set(domainElement, binaryFunction.valueAt(
+                    firstFuzzySet.getValueAt(domainElement), secondFuzzySet.getValueAt(domainElement)));
+        }
+        return resultFuzzySet;
+    }
+
+    public static IUnaryFunction zadehNot() {
+        return x -> 1 - x;
+    }
+
+    public static IBinaryFunction zadehAnd() {
+        return Math::min;
+    }
+
+    public static IBinaryFunction zadehOr() {
+        return Math::max;
+    }
+
+    //todo
+    public static IBinaryFunction hamacherTNorm(double x) {
         return null;
     }
 
     //todo
-    public static IUnaryFunction zadehNot(){
-        return null;
-    }
-
-    //todo
-    public static IBinaryFunction zadehAnd(){
-        return null;
-    }
-
-    //todo
-    public static IBinaryFunction zadehOr(){
-        return null;
-    }
-
-    //todo
-    public static IBinaryFunction hamacherTNorm(double x){
-        return null;
-    }
-
-    //todo
-    public static IBinaryFunction hamacherSNorm(double x){
+    public static IBinaryFunction hamacherSNorm(double x) {
         return null;
     }
 
