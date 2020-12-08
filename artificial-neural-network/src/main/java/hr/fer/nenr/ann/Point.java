dfdf713ab4356a01cfdb1a40543ec185810b900e
Stela Gaši <sg50985@fer.hr>
2020-12-08 23:38:00 +0100

    added Point

---
+++ artificial-neural-network/src/main/java/hr/fer/nenr/ann/Point.java
@@ -0,0 +1,35 @@
+package hr.fer.nenr.ann;
+
+import java.util.Objects;
+
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return Double.compare(point.x, x) == 0 &&
+                Double.compare(point.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
